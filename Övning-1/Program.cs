68a2c973da87aa485dadc118b05c0b57cde405ae
Alexander <alex.esbj@gmail.com>
2019-09-20 15:05:47 +0200

    Struktur
    Skapade mappstruktur och konsoler i alla undermappar som är uppgifter

---
+++ Övning-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Övning_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
