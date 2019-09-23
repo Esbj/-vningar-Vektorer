fcdac926af03647bfb798d939daed6fd7f184886
Alexander <alex.esbj@gmail.com>
2019-09-23 15:15:19 +0200

    Del 2 nästan klar
    Behöver felhantera med try catch eller ev. throw catch. Try convertToInt32?

--- Övning-2/Program.cs
+++ Övning-2/Program.cs
@@ -6,7 +6,21 @@ namespace Övning_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Steg 2 - Skapa en string array med fem element. Användaren får mata in fem namn
+            // via en for-loop. Programmet skriver sedan ut dessa namn med hjälp av en for-loop.
+            string [] namn = new string[5];
+                Console.WriteLine("Mata in 5 namn");
+                for(int i =0; i < namn.Length; i++){
+                    namn[i]=Console.ReadLine();
+                    if (namn[i].Length == 1){
+                        //felmeddelande med throw?
+                    }
+                }
+
+
+                for(int j = 0; j < namn.Length; j++){
+                    Console.WriteLine("Namn: "+ (j+1) +" är: "+namn[j]);
+                }
         }
     }
 }
