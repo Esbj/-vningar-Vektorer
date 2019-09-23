294fee03f525b0cb5e6cc8987ee1a528ae525945
Alexander <alex.esbj@gmail.com>
2019-09-23 14:16:37 +0200

    Gjorde klart del 1
    Kan putsas upp, går ev. tillbaka och fixar mer DRY kod. T.ex. lägg inmatningen i loop.

--- Övning-1/Program.cs
+++ Övning-1/Program.cs
@@ -6,17 +6,30 @@ namespace Övning_1
     {
         static void Main(string[] args)
         {
-            int tal1 = Convert.ToInt32(Console.ReadLine());
-            int tal2 = Convert.ToInt32(Console.ReadLine());
-            int tal3 = Convert.ToInt32(Console.ReadLine());
-            int tal4 = Convert.ToInt32(Console.ReadLine());
-            int tal5 = Convert.ToInt32(Console.ReadLine());
+            try{
+                Console.WriteLine("Mata in 5 heltal: ");
+                int tal1 = Convert.ToInt32(Console.ReadLine());
+                int tal2 = Convert.ToInt32(Console.ReadLine());
+                int tal3 = Convert.ToInt32(Console.ReadLine());
+                int tal4 = Convert.ToInt32(Console.ReadLine());
+                int tal5 = Convert.ToInt32(Console.ReadLine());
 
-            int[] numberHolder {} 
-            // Syntax array:
-            // type [ ] < Name_Array > = new < datatype > [size];
-            for (int i = 0; true ;){
-                
+                // Syntax array:
+                // type [ ] < Name_Array > = new < datatype > [size];
+                // int[] array3 = { 1, 2, 3, 4, 5, 6 };
+                // int [] numberHolder = new int[5];
+
+                int resultNumber = 0;
+
+                int[] numberHolder = {tal1, tal2, tal3, tal4, tal5};
+                for (int i = 0; i < numberHolder.Length ;i++){
+                    int previousNumber = numberHolder [i];
+                    Console.WriteLine("Tal "+(i+1)+": "+numberHolder[i]);
+                    resultNumber = resultNumber + previousNumber;
+                }
+                Console.WriteLine("Summan av alla tal är "+resultNumber);
+            }catch{
+                Console.WriteLine("Du matade inte in ett heltal med siffror (1, 2, 3 etc..)");
             }
         }
     }
