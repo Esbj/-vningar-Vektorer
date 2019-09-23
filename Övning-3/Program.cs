6788e33dd89221fe31e4fd801c79678b416c944a
Alexander <alex.esbj@gmail.com>
2019-09-23 17:24:41 +0200

    Lade till beskrivning

--- Övning-3/Program.cs
+++ Övning-3/Program.cs
@@ -6,7 +6,11 @@ namespace Övning_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Efter att alla namn skrivits ut, ska användaren kunna skriva ett index och
+            // därefter ett annat namn. Personen på detta index i vektorn byts då ut och alla
+            // namn skrivs ut igen.Om användaren istället skriver ”q” eller ”Q” så avslutas
+            // programmet direkt.
+            
         }
     }
 }
